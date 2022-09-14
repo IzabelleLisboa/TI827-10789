0baf8591b20ac9752cff9f43450b320f8a9f468a
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-09-14 12:05:35 +0100

    função comprimentos

--- Intro JAVA/src/intro/java2/IntroJAVA.java
+++ Intro JAVA/src/intro/java2/IntroJAVA.java
@@ -1,20 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package intro.java;
+package intro.java2;
 
-/**
- *
- * @author ilisboa
- */
+import java.util.Scanner;
+
+
 public class IntroJAVA {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner ler = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+            
+            int op;
+            do{
+            System.out.println( "Menu Conversões");
+            System.out.println( "1 - Comprimento");
+            System.out.println( "2 - Pesos");
+            System.out.println("3 - Moedas");
+            System.out.println("$ - Sair");
+            System.out.println("Escolha a opção pretendida");
+            op = ler.nextInt();
+            if(op==1)
+                convertComprimento();
+            else if(op== 2)
+                convertePesos();
+            else if(op== 3)
+                converteMoedas();
+            else if (op!=4)
+                System.out.print("Escolha uma opção válida");
+            }while(op!=4);
+                
     }
-    
-}
+
+    private static void convertComprimento() {
+      float metros;
+        System.out.println( "Insira o valor em metros");
+        metros = ler.nextFloat();
+        System.out.println(metros+" (m) equivale a:\n"+(metros*10)+"(dm)\n"+(metros*100)+"(cm)\n"+
+                +(metros*1000)+"(mm)\n"+
+                +(metros/1000)+"(km)"); 
+        
+    }
+
+    private static void convertePesos() {
+    }
+
+    private static void converteMoedas() {
+        
+    }
+    }
